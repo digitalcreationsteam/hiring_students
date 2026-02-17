11e517949bccf3914fd4ac40f5e94d038fb65981
digitalcreationsteam <dcteam1999@gmail.com>
2026-02-17 19:25:58 +0530

    Update API.js

--- src/common/API.js
+++ src/common/API.js
@@ -8,11 +8,11 @@ import axios from "axios";
 ========================================= */
 // export const BASE_URL = "http://localhost:5000/dev-api";
 // export const BASE_URL = "http://localhost:5000/api";
-// export const BASE_URL = "https://unitalent.cloud/api";
+export const BASE_URL = "https://unitalent.cloud/api";
 // export const BASE_URL = "http://localhost:5000/api";
 // export const BASE_URL = "http://192.168.0.105:5001/api";
 // export const BASE_URL = "http://192.168.0.104:5002/api";
-export const BASE_URL = "https://unitalent.cloud/dev-api";
+// export const BASE_URL = "https://unitalent.cloud/dev-api";
 
 
 const isDev = process.env.NODE_ENV === "development";
